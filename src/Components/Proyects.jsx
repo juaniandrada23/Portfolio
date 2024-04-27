216ca14d550e544b1574a4e94e1232e86cf35490
juaniandrada23 <juanandrada_07_@hotmail.com>
2024-04-27 11:43:38 -0300

    Agregando componente proyectos

--- src/Components/Proyects.jsx
+++ src/Components/Proyects.jsx
@@ -7,17 +7,17 @@ const Proyects = () => {
             <div className='flex md:justify-start py-6 xs:justify-center xs:items-center bg-gradient-to-l from-reno-950 to-gray-800 h-32'>
                 <span className='text-4xl mt-4 md:ml-5 xs:my-2 font-semibold text-reno-50'>Proyectos</span>
             </div>
-            <div className='w-full h-full grid md:grid-cols-4 xs:grid-cols-1'>
-              <div className='bg-reno-50 h-full flex justify-center items-center'>
+            <div className='w-full h-full grid md:grid-cols-4 xs:grid-cols-1 my-10'>
+              <div className='bg-reno-50 h-full flex justify-center items-center skew-y-12 transition hover:skew-y-0 hover:duration-700'>
                 <h1 className='text-reno-900'>Columna 1</h1>
               </div>
-              <div className='bg-reno-100 h-full flex justify-center items-center'>
+              <div className='bg-reno-100 h-full flex justify-center items-center skew-y-12 transition hover:skew-y-0 hover:duration-700'>
                 <h1 className='text-reno-900'>Columna 2</h1>
               </div>
-              <div className='bg-reno-200 h-full flex justify-center items-center'>
+              <div className='bg-reno-200 h-full flex justify-center items-center skew-y-12 transition hover:skew-y-0 hover:duration-700'>
                 <h1 className='text-reno-900'>Columna 3</h1>
               </div>
-              <div className='bg-reno-300 h-full flex justify-center items-center'>
+              <div className='bg-reno-300 h-full flex justify-center items-center skew-y-12 transition hover:skew-y-0 hover:duration-700'>
                 <h1 className='text-reno-900'>Columna 4</h1>
               </div>
             </div>
